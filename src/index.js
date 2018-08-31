7090100c8cd4fc3a95ba86819a284d3983bdfcc8
Gudberg Sumarlidason <gudberg_sumarlidason@Gudberg.local>
2018-08-31 16:09:19 +0000

    intiializing the project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 import React, {Component} from 'react'
 
-export default class extends Component {
+class Button extends Component {
+  
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    return(
+      <div>
+        <button style={theme}>{this.props.text}</button>
+      </div>
+    )
   }
 }
+
+Button.defaultProps = {
+  theme: {
+    color: 'white',
+    display: '1.2em 2.9em',
+    width: 180,
+    fontSize: 14,
+    margin: '0 0.5em 1.9em',
+    borderRadius: '0.4em',
+    boxSizing: 'border-box',
+    textDecoration: 'none',
+    fontFamily: 'Roboto sans-serif',
+    fontWeight: 400,
+    color: '#FFFFFF',
+    backgroundColor: '#3AA0FF',
+    textAlign: 'center',
+    position: 'relative',
+    border: 'none'
+  },
+  text: 'Hello'
+};
+
+
+
+
+export default Button
