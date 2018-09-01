185bf8dbd62ef37b3cbc2cf61a7f803c240c4d77
Gudberg Sumarlidason <gudberg_sumarlidason@Gudberg.local>
2018-09-01 18:36:26 +0000

    allows text, color, type, href, onclick and disabled

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,14 +1,30 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
-
-import Example from '../../src'
+import Button from '../../src'
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>react-button Demo</h1>
-      <Example/>
+    return (
+     
+     <div>
+        <h1>react-button Demo</h1>
+        <Button
+        text={'Submit'} 
+        type={'success'}
+        tag={'a'}
+        href={'http://www.bbc.com/'}
+        onClick={() => {}}
+        disabled={true}
+      />
+
+      <Button
+        size={'medium'}
+        text={'Cancel'}
+        type={'ded'}
+        tag={'button'}
+    />
     </div>
+  )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,38 +1,58 @@
 import React, {Component} from 'react'
+import styles from './index.css'
 
-class Button extends Component {
+class Button extends Component { 
+  
+  constructor(props) {
+    super(props)
+    this.handleOnClick = this.handleOnClick.bind(this)
+  }
+
+  handleOnClick(e) {
+    
+    if(this.props.disabled) {
+      e.preventDefault()
+      return
+    }
+
+    if(this.props.onClick) {
+      this.props.onClick(e);
+    }
+  }
   
   render() {
+    let {
+      size,
+      type,
+      text, 
+      disabled,
+      tag: Tag,
+      ...rest
+    } = this.props
+
+    if (rest.href && Tag === 'button') {
+      Tag = 'a';
+    }
+
     return(
-      <div>
-        <button style={theme}>{this.props.text}</button>
-      </div>
+      <Tag
+        type={(Tag === 'button' && rest.onClick) ? 'button' : undefined}
+        {...rest}
+        className={`${type} ${disabled ? 'disabled': null} ${size} button`}
+        onClick={this.onClick}
+      >
+      {text}
+      </Tag>
     )
   }
+  
 }
 
-Button.defaultProps = {
-  theme: {
-    color: 'white',
-    display: '1.2em 2.9em',
-    width: 180,
-    fontSize: 14,
-    margin: '0 0.5em 1.9em',
-    borderRadius: '0.4em',
-    boxSizing: 'border-box',
-    textDecoration: 'none',
-    fontFamily: 'Roboto sans-serif',
-    fontWeight: 400,
-    color: '#FFFFFF',
-    backgroundColor: '#3AA0FF',
-    textAlign: 'center',
-    position: 'relative',
-    border: 'none'
-  },
-  text: 'Hello'
-};
-
-
-
+Button.defaultProps = {
+  size: 'medium',
+  type: 'primary',
+  tag: 'button',
+  onClick: () => {}
+}
 
 export default Button
